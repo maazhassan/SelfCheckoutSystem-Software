54d9d4aa369f401c9ad79bf551982c826f7f52d7
FireHeart2K <36017101+FireHeart2K@users.noreply.github.com>
2021-03-17 21:46:45 -0400

    Update BanknoteSlotController.java

--- src/BanknoteSlotController.java
+++ src/BanknoteSlotController.java
@@ -12,33 +12,26 @@ public class BanknoteSlotController implements  BanknoteSlotListener {
 
     @Override
     public void banknoteInserted(BanknoteSlot slot) {
-        System.out.println("A " + slot + "hasn been enabled");
+        System.out.println("A " + slot + "was inserted");
     }
 
     @Override
     public void banknoteEjected(BanknoteSlot slot) {
-        System.out.println("A " + slot + "hasn been enabled");
+        System.out.println("A " + slot + "was ejected");
     }
 
     @Override
-    public void banknoteRemoved(BanknoteSlot slot) {
-        System.out.println("A " + slot + "hasn been disabled");
-
-    }
-
-    @Override
-    public void coinInserted(CoinSlot slot)  {
-        System.out.println("A coin was inserted.");
+    public void banknoteRemoved(BanknoteSlot slot){
+        System.out.println("A " + slot + " was removed");
     }
 
     @Override
     public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
-        System.out.println("A " + device.getClass().getSimpleName() + " has been enabled.");
+        System.out.println("A " + device.getClass().getSimpleName() + " was inserted.");
     }
-
 
     @Override
     public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
-        System.out.println("A " + device.getClass().getSimpleName() + " has been disabled.");
+        System.out.println("A " + device.getClass().getSimpleName() + " was eject.");
     }
 }
